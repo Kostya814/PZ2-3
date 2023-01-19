1001c365d8a98ff7bb0c4a748a7739b851dbd688
John Doe <gnil47311@gmail.com>
2023-01-19 08:37:01 +0700

    Добавьте файлы проекта.

---
+++ PZ2-3/Product.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ2_3
+{
+    class Product
+    {
+        private int _first;
+        private double _second;
+        public Product(int first, double second) 
+        {
+            _first = first;
+            _second = second;
+        }    
+       
+        public int First 
+        { 
+            get 
+            { 
+                return _first; 
+            } 
+            set 
+            { 
+                if (value>0)
+                    _first = value;
+            } 
+        }
+        
+        public double Second
+        {
+            get
+            {
+                return _second;
+            }
+            set
+            {
+                if (value > 0)
+                    _second = value;
+            }
+        }
+        public void power()
+        {
+            double sum = (_second*10) * _first; 
+        }
+        public void Read() 
+        {
+            _first = Convert.ToInt32(Console.ReadLine());
+            _second = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void Display() 
+        {
+            Console.WriteLine($"Калорийность продукта на 100 г {_first} \n Масса продукта {_second}" );
+        }
+
+    }
+}
